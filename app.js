7d5fdd8f319f9a424cc9ba2aec1415c1acf45d13
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-06-03 13:33:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Search Input
+
+const SearchUser = document.getElementById('searchUser');
+
+// Search Input Event Listener
+
+SearchUser.addEventListener('keyup', (e) => {
+    // Get Input text
+
+    const userText = e.target.value;
+
+    if (userText !== '') {}
+})
---
+++ github.js
@@ -0,0 +1,5 @@
+class GitHUb {
+    constructor() {
+        this.client_id = '51d07af851cb027fafa4'
+    }
+}
